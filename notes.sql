dd0cd53ec884c5a858a680db986efe641c4eceab
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-10-18 13:06:12 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -5,14 +5,17 @@
 /* 0 SELECT basics */
 
 --IN checks if an item is in a list.
---Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
-SELECT name, population
+SELECT name, population --Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 FROM world
-WHERE name IN ('Sweden', 'Norway', 'Denmark')
+WHERE name IN ('Sweden', 'Norway', 'Denmark') 
 
 --BETWEEN allows range checking (range specified is inclusive of boundary values)
 SELECT name, area 
 FROM world
 WHERE area BETWEEN 200000 AND 250000
 
---1 SELECT names
+/* 1 SELECT names */
+
+
+
+
