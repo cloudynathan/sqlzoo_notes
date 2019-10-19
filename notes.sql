98e4ca3b4638de4902edf6f32f6f375d7b4effe9
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-10-18 17:18:20 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -51,5 +51,26 @@ WHERE capital LIKE concat('%', name, '%') AND capital > name
 FROM world
 WHERE capital LIKE concat('%', name, '%') AND capital > name
 
-/*---------- 2. SELECT nobel */
+/*---------- 2. SELECT world */
+--3. Give the name and the per capita GDP for those countries with a population of at least 200 million.
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
+
+--7. Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+--Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area
+FROM world
+WHERE (area >= 3000000) OR (population > 250000000)
+
+--XOR (Exclusive OR) 
+--8. Show the countries that are big by area or big by population but not both. Show name, population and area.
+--Australia has a big area but a small population, it should be included.
+--Indonesia has a big population but a small area, it should be included.
+--China has a big population and big area, it should be excluded.
+--United Kingdom has a small population and a small area, it should be excluded.
+SELECT name, population, area
+FROM world
+WHERE (area >= 3000000) XOR (population > 250000000)
+
 
