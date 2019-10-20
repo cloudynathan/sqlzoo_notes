d5ae9adfb9023e0a5790c6b4c1cebea5e201a30d
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-10-20 14:45:48 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -124,7 +124,14 @@ SELECT *
 FROM nobel
 WHERE (subject = 'Literature') AND (yr BETWEEN 1980 AND 1989);
 
+--12. Find all details of the prize won by EUGENE O'NEILL.
+SELECT *
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL'; --use double ' to escape
 
-
-
-
+-- 13. List the winners, year and subject where the winner starts with 'Sir'. 
+--Show the the most recent first, then by name order.
+SELECT winner, yr, subject
+FROM nobel
+WHERE (winner LIKE 'Sir%')
+ORDER BY yr DESC, winner;
