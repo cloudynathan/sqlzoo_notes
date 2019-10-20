5e45f14c480de0ee715ec795d1acd8599d98c379
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-10-20 14:11:50 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -119,6 +119,10 @@ name LIKE '%u%' AND
 name NOT LIKE '% %';
 
 /*---------- 3. SELECT nobel */
+--5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+SELECT *
+FROM nobel
+WHERE (subject = 'Literature') AND (yr BETWEEN 1980 AND 1989);
 
 
 
