b91367cb519d7a176879bf176f1d5cad00affd85
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-10-28 16:37:46 -0700

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -227,7 +227,7 @@ WHERE teamid='GER';
 --6. List the the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
 SELECT mdate, teamname
 FROM game JOIN eteam ON (game.team1 = eteam.id)
-WHERE coach = 'Fernando Santos'
+WHERE coach = 'Fernando Santos';
 
 
 
