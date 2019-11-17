4b1a150563a6623fb2decb273ced374c0fa729e9
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-11-17 10:12:18 -0800

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -300,3 +300,25 @@ ORDER BY COUNT(actorid) DESC, title;
 SELECT name 
 FROM actor JOIN casting ON actor.id=casting.actorid 
 WHERE movieid IN (10095, 11434, 13630) AND actor.name != 'Art Garfunkel';
+
+/*---------- 8. Using NULL*/
+-- IS NULL or IS NOT NULL
+-- 1. List the teachers who have NULL for their department.
+SELECT teacher.name
+FROM teacher 
+WHERE teacher.dept IS NULL;
+
+-- COALESCE function: COALESCE takes any number of arguments and returns the first value that is not null.
+-- COALESCE(x,y,z) = x if x is not NULL
+-- COALESCE(x,y,z) = y if x is NULL and y is not NULL
+-- COALESCE(x,y,z) = z if x and y are NULL but z is not NULL
+-- COALESCE(x,y,z) = NULL if x and y and z are all NULL
+
+-- COALESCE can be useful when you want to replace a NULL value with some other value. 
+-- In this example you show the name of the party for each MSP that has a party. 
+-- For the MSP with no party (such as Canavan, Dennis) you show the string None.
+SELECT name, party, COALESCE(party,'None') AS aff
+FROM msp 
+WHERE name LIKE 'C%';
+
+                                                                 
