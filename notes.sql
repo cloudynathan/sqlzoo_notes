7cd7d523672729af4d4df049c22588366afcc72f
Nathan <51245109+cloudynathan@users.noreply.github.com>
2019-11-24 12:05:52 -0800

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -321,4 +321,15 @@ SELECT name, party, COALESCE(party,'None') AS aff
 FROM msp 
 WHERE name LIKE 'C%';
 
-                                                                 
+-- CASE allows you to return different values under different conditions.
+-- If there no conditions match (and there is not ELSE) then NULL is returned.
+-- CASE WHEN condition1 THEN value1 
+--     WHEN condition2 THEN value2  
+--     ELSE def_value 
+-- END
+-- For example: 
+SELECT name, population , CASE WHEN population<1000000 THEN 'small'
+                               WHEN population<10000000 THEN 'medium'
+                               ELSE 'large' END
+FROM bbc
+
