21a5c10d76a5b17f03ccf203aa57e5f1e1472441
Nathan <51245109+cloudynathan@users.noreply.github.com>
2020-02-14 13:21:13 -0800

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -356,6 +356,13 @@ FROM ge
 WHERE constituency BETWEEN 'S14000021' AND 'S14000026' AND yr = 2017 
 ORDER BY posn, constituency, votes DESC;
 
+5. Edinburgh winners
+SELECT constituency, party 
+FROM (SELECT constituency,party, votes, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) as posn 
+      FROM ge
+      WHERE constituency BETWEEN 'S14000021' AND 'S14000026' AND yr  = 2017 
+ORDER BY posn, constituency,votes DESC) as T 
+WHERE posn = 1;
                                                                  
                                                                  
                                                                  
