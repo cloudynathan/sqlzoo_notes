619d95c2e090f8ba9ea735590766a9f1c1009d55
Nathan <51245109+cloudynathan@users.noreply.github.com>
2020-09-28 17:00:51 -0700

    Update guest_house.sql

--- guest_house.sql
+++ guest_house.sql
@@ -60,6 +60,109 @@ FROM booking b
 WHERE b.booking_date <= '2016-11-21' 
 AND DATEADD(day, nights, b.booking_date) > '2016-11-21'
 
+-- HARD:--------------------------------------------
+
+--11. 
+SELECT g.last_name, g.first_name, g2.first_name 
+FROM booking b, guest g, booking b2, guest g2 
+WHERE b.guest_id=g.id 
+AND b2.guest_id=g2.id 
+AND b.booking_date BETWEEN b2.booking_date 
+AND DATEADD (Day, b2.nights, b2.booking_date) 
+AND g.last_name=g2.last_name 
+AND g.id<g2.id 
+ORDER BY g.last_name;
+
+--12.
+SELECT checkout_date, sum(count1) AS first, sum(count2) AS second, sum(count3) AS third 
+FROM (
+ SELECT convert(varchar, dateadd(day, b.nights, b.booking_date)) AS 'checkout_date', 
+ CASE
+  WHEN LEFT(room_no, 1) = 1 THEN 1 
+ END AS count1, 
+ CASE
+  WHEN LEFT(room_no, 1) = 2 THEN 1 
+ END AS count2,
+ CASE
+  WHEN LEFT(room_no, 1) = 3 THEN 1 
+ END AS count3 
+ FROM booking b 
+ WHERE DATEADD(Day, b.nights, b.booking_date) BETWEEN '2016-11-14' AND DATEADD(Day, 7, '2016-11-14')) c 
+GROUP BY checkout_date 
+
+--intermediate step
+SELECT convert(varchar, dateadd(day, b.nights, b.booking_date)) AS 'checkout_date', 
+ room_no, 
+ CASE 
+  WHEN LEFT(room_no, 1) = 1 THEN 1 
+ END AS count1, 
+ CASE
+  WHEN LEFT(room_no, 1) = 2 THEN 1 
+ END AS count2,
+ CASE
+  WHEN LEFT(room_no, 1) = 3 THEN 1 
+ END AS count3 
+ FROM booking b 
+ WHERE DATEADD(Day, b.nights, b.booking_date) BETWEEN '2016-11-14' AND DATEADD(Day, 7, '2016-11-14') 
+
+--13.
+SELECT r.id 
+FROM room r 
+WHERE NOT EXISTS (
+ SELECT * 
+ FROM booking b 
+ WHERE b.room_no=r.id 
+ AND '2016-11-25' BETWEEN b.booking_date AND DATEADD(Day, b.nights, b.booking_date))  
+UNION 
+SELECT r.id 
+FROM room r 
+WHERE EXISTS (
+ SELECT * 
+ FROM booking b 
+ WHERE b.room_no=r.id 
+ AND DATEADD(Day, b.nights, b.booking_date)='2016-11-25' 
+ AND NOT EXISTS (
+  SELECT * 
+  FROM booking b 
+  WHERE b.room_no=r.id 
+  AND b.booking_date='2016-11-25'))
+
+--#block1: rooms where there are no checks ins 
+SELECT r.id 
+FROM room r 
+WHERE NOT EXISTS (
+ SELECT * 
+ FROM booking b 
+ WHERE b.room_no=r.id 
+ AND '2016-11-25' BETWEEN b.booking_date AND DATEADD(Day, b.nights, b.booking_date))
+
+--#block2: where people checked out on this day AND no new check ins
+SELECT r.id 
+FROM room r 
+WHERE EXISTS (
+ SELECT * 
+ FROM booking b 
+ WHERE b.room_no=r.id 
+ AND DATEADD(Day, b.nights, b.booking_date)='2016-11-25' 
+ AND NOT EXISTS (
+  SELECT * 
+  FROM booking b 
+  WHERE b.room_no=r.id 
+  AND b.booking_date='2016-11-25'))
+
+--14. 
+SELECT TOP 1 c.room_no, MAX(LEFT(c.checkout,10)) checkout 
+FROM (
+ SELECT room_no, booking_date, DATEADD(Day, b.nights, b.booking_date) AS checkout, b.nights 
+ FROM booking b, room r 
+ WHERE b.room_no=r.id 
+ AND max_occupancy=1) c 
+GROUP BY c.room_no 
+ORDER BY checkout
+
+
+
+
 
 
 
